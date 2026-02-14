3213e989fe793e7b5a52dc9fc253181e7b52c6e0
poojapatil1213 <pvipatil45@gmail.com>
2026-02-13 17:27:33 -0800

    initial commit for the python script(to be run by the end user) written for initial commit of barcode.ipynb

---
+++ generate_barcode.py
@@ -0,0 +1,7 @@
+
+def generate_series(series_start,series_prefix,series_suffix,series_counter):
+	series_start_str = str(series_start)
+	for digit_char in series_start_str:
+		digit_int = int(digit_char)
+	
+	return 0	
